04704543c1140fc56501beefca6a1303e141bd11
aceysx <aceysx@163.com>
2019-01-11 10:25:25 +0800

    Add ajax demo of jquery version.

--- js/song/listener.js
+++ js/song/listener.js
@@ -1,4 +1,21 @@
+function request(type, url, callback) {
+  $.ajax({
+    type,
+    url,
+    dataType: "jsonp",
+    success: function (json) {
+      callback(json)
+    }
+  });
+}
+
 function onloadPageListener() {
+
+  const url = 'https://api.douban.com/v2/movie/subject/26865690/photos?apikey=0b2bdeda43b5688921839c8ecb20399b&start=0&count=100&client=&udid='
+  request('get', url, (data) => {
+    console.log(data)
+  })
+
   const movies = formatMovies(data);
   setMovies(movies);
   showSortTable();
